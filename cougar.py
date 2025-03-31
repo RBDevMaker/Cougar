56d11d2a866ffab36032a487cac16e30d6fbe8fb
RBDevMaker <rachelle.briscoe@navontech.com>
2025-03-31 18:00:00 -0400

    Added cougar.py file
    I created this code to practice basic python.

---
+++ cougar.py
@@ -0,0 +1,43 @@
+feline_age = input("Which feline are you? (Enter your age): ")
+feline_age = int(feline_age)
+
+if 0 <= feline_age <= 17:
+    print ("Your a Kitten!")
+    
+if 18 <= feline_age <= 21:
+    print ("Your a Wildcat!")
+    
+if 22 <= feline_age <= 29:
+    print ("Your a Lynx!")
+    
+if 30 <= feline_age <= 39:
+    print ("Your a Puma!")
+    
+if 40 <= feline_age <= 49:
+    print ("Your a Cougar!")
+
+if 50 <= feline_age <= 59:
+    print ("Your a Jaguar!") 
+
+if 60 <= feline_age <= 68:
+    print ("Your a Panther!")
+
+if feline_age == 69:
+    print ("Your a Pussycat!")
+    
+if 70 <= feline_age <= 79:
+    print ("Your a Cheetah!")
+
+if 80 <= feline_age <= 89:
+    print ("Your a Leopard!")
+
+if 90 <= feline_age <= 99:
+    print ("Your a Tiger!")   
+    
+if feline_age >= 100:
+    print ("Your a Lion!")
+    
+    
+    
+
+    
