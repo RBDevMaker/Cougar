6688fdf9eebb5a0bd3bdb8871d4274618e04bb73
Rachelle Briscoe <rachelle.briscoe@navontech.com>
2025-04-04 00:45:21 -0400

    Corrected grammer errors

--- cougar.py
+++ cougar.py
@@ -2,40 +2,40 @@ feline_age = input("Which feline are you? (Enter your age): ")
 feline_age = int(feline_age)
 
 if 0 <= feline_age <= 17:
-    print ("Your a Kitten!")
+    print ("You are a Kitten!")
     
 if 18 <= feline_age <= 21:
-    print ("Your a Wildcat!")
+    print ("You are a Wildcat!")
     
 if 22 <= feline_age <= 29:
-    print ("Your a Lynx!")
+    print ("You are a Lynx!")
     
 if 30 <= feline_age <= 39:
-    print ("Your a Puma!")
+    print ("You are a Puma!")
     
 if 40 <= feline_age <= 49:
-    print ("Your a Cougar!")
+    print ("You are a Cougar!")
 
 if 50 <= feline_age <= 59:
-    print ("Your a Jaguar!") 
+    print ("You are a Jaguar!") 
 
 if 60 <= feline_age <= 68:
-    print ("Your a Panther!")
+    print ("You are a Panther!")
 
 if feline_age == 69:
-    print ("Your a Pussycat!")
+    print ("You are a Pussycat!")
     
 if 70 <= feline_age <= 79:
-    print ("Your a Cheetah!")
+    print ("You are a Cheetah!")
 
 if 80 <= feline_age <= 89:
-    print ("Your a Leopard!")
+    print ("You are a Leopard!")
 
 if 90 <= feline_age <= 99:
-    print ("Your a Tiger!")   
+    print ("You are a Tiger!")   
     
 if feline_age >= 100:
-    print ("Your a Lion!")
+    print ("You are a Lion!")
     
     
     
