8bfb08f34c26c0b7af1418ca21adbb8245b3704a
Rachelle Briscoe <rachelle.briscoe@navontech.com>
2025-04-04 01:58:58 -0400

    grammer syntax

--- cougar.py
+++ cougar.py
@@ -1,43 +1,45 @@
-feline_age = input("Which feline are you? (Enter your age): ")
-feline_age = int(feline_age)
+while True:
+    try:
 
-if 0 <= feline_age <= 17:
-    print ("Your a Kitten!")
+        feline_age = int(input("Which feline are you? (Enter your age): "))
+
+        if 0 <= feline_age <= 17:
+            print ("You are a Kitten!")
     
-if 18 <= feline_age <= 21:
-    print ("Your a Wildcat!")
+        elif 18 <= feline_age <= 21:
+            print ("You are a Wildcat!")
     
-if 22 <= feline_age <= 29:
-    print ("Your a Lynx!")
+        elif 22 <= feline_age <= 29:
+            print ("You are a Lynx!")
     
-if 30 <= feline_age <= 39:
-    print ("Your a Puma!")
+        elif 30 <= feline_age <= 39:
+            print ("You are a Puma!")
     
-if 40 <= feline_age <= 49:
-    print ("Your a Cougar!")
+        elif 40 <= feline_age <= 49:
+            print ("You are a Cougar!")
 
-if 50 <= feline_age <= 59:
-    print ("Your a Jaguar!") 
+        elif 50 <= feline_age <= 59:
+            print ("You are a Jaguar!") 
 
-if 60 <= feline_age <= 68:
-    print ("Your a Panther!")
+        elif 60 <= feline_age <= 68:
+            print ("You are a Panther!")
 
-if feline_age == 69:
-    print ("Your a Pussycat!")
+        elif feline_age == 69:
+            print ("You are a Pussycat!")
     
-if 70 <= feline_age <= 79:
-    print ("Your a Cheetah!")
+        elif 70 <= feline_age <= 79:
+            print ("You are a Cheetah!")
 
-if 80 <= feline_age <= 89:
-    print ("Your a Leopard!")
+        elif 80 <= feline_age <= 89:
+            print ("You are a Leopard!")
 
-if 90 <= feline_age <= 99:
-    print ("Your a Tiger!")   
+        elif 90 <= feline_age <= 99:
+            print ("You are a Tiger!")   
     
-if feline_age >= 100:
-    print ("Your a Lion!")
+        elif feline_age >= 100:
+            print ("You are a Lion!")
 
-else:
+        else:
             print("Sorry, I can only categorize ages from 0 to 100.")
         
        # Ask again for age after displaying the category
